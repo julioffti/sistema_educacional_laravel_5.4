0a3dc42736e687d4484d4e41cdf5badcd369bce1
julio <julio_filgueiras@live.com>
2017-12-10 14:22:22 -0200

    Acessando com e-mail ou matrícula

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace CONDER\Http\Controllers\Auth;
 
 use CONDER\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,22 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function credentials(Request $request)
+    {
+        $data = $request->only($this->username(),'password');
+        $usernameKey = $this->usernameKey();
+        $data[$usernameKey] = $data[$this->username()];
+        unset($data[$this->username()]);
+        return $data;
+    }
+
+    protected function usernameKey(){
+        $email = \Request::get($this->username());
+        $validator = \Validator::make([
+            'email' => $email
+        ], ['email' => 'email']);
+        return $validator->fails() ? 'enrolment' : 'email';
     }
 }
