86d704a07c3e15f25e3b06e731384f9fe0c0a96e
julio <julio_filgueiras@live.com>
2017-12-27 17:05:53 -0200

    prosseguindo com criação de tipos de usuários

--- app/Forms/UserForm.php
+++ app/Forms/UserForm.php
@@ -2,6 +2,7 @@
 
 namespace CONDER\Forms;
 
+use CONDER\Models\User;
 use Kris\LaravelFormBuilder\Form;
 
 class UserForm extends Form
@@ -18,10 +19,23 @@ class UserForm extends Form
                 'label' => 'E-mail',
                 'rules' => "required|max:255|unique:users,email,{$id}"
             ])
+            ->add('type', 'select',[
+                'label' => 'Tipo de usuário',
+                'choices' => $this->roles(),
+                'rules' => 'required|in:'.implode(',', array_keys($this->roles()))
+            ])
             ->add('send_mail', 'checkbox', [
                'label' => 'Enviar e-mail de boas vindas',
                 'value' => true,
                 'checked' => false
              ]);
+    }
+
+    protected function roles(){
+        return[
+            User::ROLE_ADMIN => 'Administrador',
+            User::ROLE_TEACHER => 'Professor',
+            User::ROLE_STUDENT => 'Aluno'
+        ];
     }
 }
